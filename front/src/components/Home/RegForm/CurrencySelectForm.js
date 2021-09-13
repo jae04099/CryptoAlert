47d7336c1a6a68fec2f978d88980fb94e6a662ab
Gyuri Lee <jae04099@catholic.ac.kr>
2021-09-13 16:53:57 +0900

    remove sort function

--- front/src/components/Home/RegForm/CurrencySelectForm.js
+++ front/src/components/Home/RegForm/CurrencySelectForm.js
@@ -42,7 +42,7 @@ function CurrencySelectForm(props){
             <div className="relative z-0 w-full mb-5">
                         <div className="px-0">
                             <select name= "coinIdx" className="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer" key={coinIdx} onChange={handleChange}>
-                                {_.sortBy(coinNameList, 'korean_name').map((coin, index) => (<option key={index} value={coin.market}>{coin.korean_name}({coin.market})</option>))}
+                                {coinNameList.map((coin, index) => (<option key={index} value={coin.market}>{coin.korean_name}({coin.market})</option>))}
                             </select>
                         </div>
                     </div>
